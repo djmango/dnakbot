0a90469df974ee63eb607389567a3ecdf5d61fc3
Sulaiman Ghori <anoneemousehax@gmail.com>
2017-07-03 22:03:07 -0400

    updated for vexdb base

--- index.js
+++ index.js
@@ -3,36 +3,57 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = "MzMxMTQyMjU4NDU3ODM3NTcw.DDrUxA.aivQIrt9x8Y1rKS8BvMf_-PwTvg";
 const prefix = "./"
+const request = require("request")
 
-bot.on("ready", function(){
+//request test
+var url = "https://api.vexdb.io/v1/get_teams?team=6096A"
+var teamdata = 0;
+
+request({
+    url: url,
+    json: true
+}, function getteamdata (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log(body) // Print the json response
+        teamdata = body;
+    }
+
+})
+
+bot.on("ready", function(){ //if ready, say so
     console.log("Ready")
 });
 
-bot.on('message', (message) => { //check message
-    if (message.author.equals(bot.user)) return;
+bot.on('message', (message) => { //check for message
+    if (message.author.equals(bot.user)) return; //check if the bot sent the message, if so ignore
 
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return; //check for prefix
 
     var args = message.content.substring(prefix.length).split(" "); //take each argument
     switch (args[0]) {
+      //reply statements
       case "ping":
-        message.channel.sendMessage('pong');
+        message.channel.send('pong');
         break;
       case "pong":
-        message.channel.sendMessage('ping');
+        message.channel.send('ping');
         break;
       case "ding":
-        message.channel.sendMessage('dong');
+        message.channel.send('dong');
         break;
       case "dong":
-        message.channel.sendMessage('ding');
+        message.channel.send('ding');
         break;
       case "bangle":
-          message.channel.sendMessage('shangle');
+          message.channel.send('shangle');
           break;
       case "shangle":
-          message.channel.sendMessage('bangle');
+          message.channel.send('bangle');
           break;
+      //vexdb commands
+      case "teamlog":
+        message.channel.send('test:', teamdata)
+        break;
     default:
         message.channel.send("Incorrect command");
     }
