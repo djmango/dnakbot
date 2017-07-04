f835829b41b9e9b10b7d48854cc019d9c8a54651
Sulaiman Ghori <anoneemousehax@gmail.com>
2017-07-04 14:24:59 -0400

    updates

--- index.js
+++ index.js
@@ -6,31 +6,35 @@ const prefix = "./"
 const request = require("request")
 
 //request test
-var url = "https://api.vexdb.io/v1/get_teams?team=6096A"
+//var url = "https://api.vexdb.io/v1/get_teams?team=6096A"
 var teamdata = 0;
-
-request({
-    url: url,
-    json: true
-}, function getteamdata (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        console.log(body) // Print the json response
-        teamdata = body;
-    }
-
-})
+var teamquery = "6096A";
 
 bot.on("ready", function(){ //if ready, say so
     console.log("Ready")
 });
-
+function requestdata(url){
+  request({
+      url: url,
+      json: true
+  }, function getteamdata (error, response, body) {
+      if (!error && response.statusCode === 200) {
+          var testbody = JSON.stringify(body);
+          teamdata = JSON.stringify(body);
+          //message.channel.send(teststring);
+        }
+      })
+}
 bot.on('message', (message) => { //check for message
     if (message.author.equals(bot.user)) return; //check if the bot sent the message, if so ignore
 
     if (!message.content.startsWith(prefix)) return; //check for prefix
 
     var args = message.content.substring(prefix.length).split(" "); //take each argument
-    switch (args[0]) {
+    function sendteamdata () {
+      message.channel.send(teamdata)
+    }
+    switch (args[0].toLowerCase()) {
       //reply statements
       case "ping":
         message.channel.send('pong');
@@ -51,19 +55,18 @@ bot.on('message', (message) => { //check for message
           message.channel.send('bangle');
           break;
       //vexdb commands
-      case "teamlog":
-        request({
-            url: url,
-            json: true
-        }, function getteamdata (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var testbody = JSON.stringify(body);
-                message.channel.send(testbody)
-                teamdata = (body);
-              }
-            })
+      case "vex":
+        switch (args[1]) {
+          case "team":
+            requestdata(url = `https://api.vexdb.io/v1/get_teams?team=${args[2]}`);
+            setTimeout(sendteamdata, 1000);
+            break;
+
+          default:
+              message.channel.send("Incorrect command, use ./vex team (team number)");
+        }
         break;
-    default:
+    default: //default
         message.channel.send("Incorrect command");
     }
 });
